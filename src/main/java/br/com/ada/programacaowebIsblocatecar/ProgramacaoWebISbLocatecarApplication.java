81f9e86ed5783c08a008b9739a4b74b7c400096f
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 17:59:30 -0300

    commit inicial

---
+++ src/main/java/br/com/ada/programacaowebIsblocatecar/ProgramacaoWebISbLocatecarApplication.java
@@ -0,0 +1,13 @@
+package br.com.ada.programacaowebIsblocatecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacaoWebISbLocatecarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacaoWebISbLocatecarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ada/programacaowebIsblocatecar/ProgramacaoWebISbLocatecarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.programacaowebIsblocatecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacaoWebISbLocatecarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
