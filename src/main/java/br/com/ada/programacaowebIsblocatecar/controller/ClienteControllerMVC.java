e0ef911e30e4e50fc584e28971c6c751e37fc115
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 22:16:51 -0300

    adiciona arquivo para controller

--- src/main/java/br/com/ada/programacaowebIsblocatecar/controller/ClienteControllerMVC.java
+++ src/main/java/br/com/ada/programacaowebIsblocatecar/controller/ClienteControllerMVC.java
@@ -1,4 +1,11 @@
 package br.com.ada.programacaowebIsblocatecar.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class ClienteControllerMVC {
 }
