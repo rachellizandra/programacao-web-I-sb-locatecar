ef1d3132c04aa592957aef38dab8db382f32c372
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 23:13:30 -0300

    altera repository

--- src/main/java/br/com/ada/programacaowebIsblocatecar/repository/ClienteRepository.java
+++ src/main/java/br/com/ada/programacaowebIsblocatecar/repository/ClienteRepository.java
@@ -1,4 +1,10 @@
 package br.com.ada.programacaowebIsblocatecar.repository;
 
-public interface ClienteRepository {
+import br.com.ada.programacaowebIsblocatecar.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByNumeroDocumento(String numeroDocumento);
 }
