28970703ba219188e79e0613d99df8f2c57d812d
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 22:17:44 -0300

    adiciona model para Cliente

--- src/main/java/br/com/ada/programacaowebIsblocatecar/model/Cliente.java
+++ src/main/java/br/com/ada/programacaowebIsblocatecar/model/Cliente.java
@@ -1,4 +1,24 @@
 package br.com.ada.programacaowebIsblocatecar.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tb_cliente")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String tipoDocumento;
+    private String numeroDocumento;
+    private String telefone;
+    private String endereco;
+
 }
