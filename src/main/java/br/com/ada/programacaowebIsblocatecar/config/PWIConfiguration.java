3a9beb72009e7d0b8fff4227a34349b092c6a484
Rachel Lizandra <rachellizandra@outlook.com>
2023-03-06 21:48:13 -0300

    adiciona configuração para getCLiente()

--- src/main/java/br/com/ada/programacaowebIsblocatecar/config/PWIConfiguration.java
+++ src/main/java/br/com/ada/programacaowebIsblocatecar/config/PWIConfiguration.java
@@ -1,4 +1,14 @@
 package br.com.ada.programacaowebIsblocatecar.config;
 
+import br.com.ada.programacaowebIsblocatecar.model.Cliente;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PWIConfiguration {
+    
+    @Bean
+    public Cliente getCLiente() {
+        return new Cliente();
+    }
 }
