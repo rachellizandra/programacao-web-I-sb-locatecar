b21316e26737a1156e58dd1bce86d073612a6252
thamiress-t <thamirest.ufpe@gmail.com>
2023-03-10 00:36:35 -0300

    altera model

--- src/main/java/br/com/ada/programacaowebIsblocatecar/model/Cliente.java
+++ src/main/java/br/com/ada/programacaowebIsblocatecar/model/Cliente.java
@@ -1,4 +1,6 @@
 package br.com.ada.programacaowebIsblocatecar.model;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -14,12 +16,27 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Esse campo não pode estar vazio")
     private String nome;
+
+    @NotBlank(message = "Esse campo não pode estar vazio")
+    @Email
     private String email;
+
+    @NotBlank(message = "Esse campo não pode estar vazio")
     private String tipoDocumento;
+
+    @NotBlank(message = "Esse campo não pode estar vazio")
     @Column(unique = true)
+
+    @NotBlank(message = "Esse campo não pode estar vazio")
     private String numeroDocumento;
+
+    @NotBlank(message = "Esse campo não pode estar vazio")
     private String telefone;
+
+    @NotBlank(message = "Esse campo não pode estar vazio")
     private String endereco;
 
 }
